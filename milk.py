f6f4c9089c135c62a11a96f1d983832a0db1f60d
MagdalenaKrck <magdalena.kreckova@gmail.com>
2024-05-16 10:32:31 +0200

    změna

--- milk.py
+++ milk.py
@@ -12,8 +12,9 @@ def je_otevreno(datum):
         return "Ne"
 
 #test
-datum1 =  datetime.date(2024, 4, 28)
-print(je_otevreno(datum1))
+zadej_datum = input("Zadej datum ve formátu YYYY-MM-DD:")
+datum =  datetime.datetime.strptime(zadej_datum, "%Y-%m-%d").date()
+print(je_otevreno(datum))
 
 #     datetime.date.today(): Tato metoda vrátí aktuální datum jako objekt třídy datetime.date. V podstatě získáváte aktuální datum, které lze použít k porovnání s jinými daty.
 
@@ -21,8 +22,3 @@ print(je_otevreno(datum1))
 
 #     if rozdil % 2 == 0: return "Ano" else: return "Ne": Tento blok kódu rozhoduje, zda je rozdíl mezi zadaným datem a aktuálním datem sudý či lichý. Operátor % zde provádí operaci modulo, což znamená, že vrátí zbytek po dělení prvního čísla (v tomto případě rozdil) druhým číslem (v tomto případě 2). Pokud je zbytek rovný 0, pak je rozdíl sudý, a tedy funkce vrátí "Ano". Jinak vrátí "Ne".
 
-#     datum1 = datetime.date(2024, 4, 27): Tímto krokem vytváříte nový objekt třídy datetime.date, který představuje zadané datum (27. dubna 2024).
-
-#     print(je_otevreno(datum1)): Nakonec voláte funkci je_otevreno() s datum1 jako argumentem a vypisujete výsledek na obrazovku.
-
-# Takže celý postup získá aktuální datum, spočítej rozdíl mezi zadaným datem a aktuálním datem v počtu dnů a poté rozhodni, zda je tento rozdíl sudý či lichý, a podle toho vrátí "Ano" nebo "Ne".
